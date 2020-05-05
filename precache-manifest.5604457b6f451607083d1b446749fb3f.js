c6a4ad151779ae78e0c6058c1ed31b6c64885865
Suraj Kamdi <suraj.kamdi@perficient.com>
2020-05-05 12:25:10 +0530

    deploying gh-pages to github environment

---
+++ precache-manifest.5604457b6f451607083d1b446749fb3f.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "d0af810874a792ae6591",
+    "url": "/learning-vue-js//css/app.32ca8004.css"
+  },
+  {
+    "revision": "82b9c7a5a3f405032b1db71a25f67021",
+    "url": "/learning-vue-js//img/logo.82b9c7a5.png"
+  },
+  {
+    "revision": "b353bda8a24408a5f97ecb5301191ef3",
+    "url": "/learning-vue-js//index.html"
+  },
+  {
+    "revision": "6ec0183c167467195bc7",
+    "url": "/learning-vue-js//js/about.260f0bb9.js"
+  },
+  {
+    "revision": "d0af810874a792ae6591",
+    "url": "/learning-vue-js//js/app.8556c936.js"
+  },
+  {
+    "revision": "dea833a8da08b0032228",
+    "url": "/learning-vue-js//js/chunk-vendors.cc40c79f.js"
+  },
+  {
+    "revision": "41011972483d15af719d70395a8b0263",
+    "url": "/learning-vue-js//manifest.json"
+  },
+  {
+    "revision": "b6216d61c03e6ce0c9aea6ca7808f7ca",
+    "url": "/learning-vue-js//robots.txt"
+  }
+]);
